5d4451ac00eaab9f793a86a637cf201b2652e8d4
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-06-02 00:57:10 +0900

    feat : Learn about JSX
    function Component, element, JSX

--- my-app/src/chapter_03/Library.jsx
+++ my-app/src/chapter_03/Library.jsx
@@ -4,9 +4,9 @@ import Book from './Book';
 function Library(props){
     return(
         <div>
-            <Book name="처음 만난 파이썬" numOfPage={300} />
-            <Book name="처음 만난 AWS" numOfPage={400} />
-            <Book name="처음 만난 리액트" numOfPage={500} />
+            <Book name="처음 만난 파이썬" numOfPage={200} />
+            <Book name="처음 만난 AWS" numOfPage={300} />
+            <Book name="처음 만난 리액트" numOfPage={400} />
         </div>
     );
 }
